5fd14b0117bbdd5d1fb077f103ca84ef4f0d2a87
Blaine Venturine <blaineventurine@gmail.com>
2020-05-14 19:45:36 -0500

    massive update, added gitlab, paperless, postgres, etc

--- start-containers.sh
+++ start-containers.sh
@@ -2,11 +2,12 @@
 docker network inspect traefik_proxy >/dev/null 2>&1 || \
   docker network create traefik_proxy
 
-COMPOSE_HTTP_TIMEOUT=120 docker-compose -f docker-compose.networks.yml \
+COMPOSE_HTTP_TIMEOUT=120 docker-compose \
+  -f docker-compose.networks.yml \
   -f docker-compose.utilities.yml \
   -f docker-compose.databases.yml \
   -f docker-compose.proxy-services.yml \
   -f docker-compose.smarthome.yml \
   -f docker-compose.information-mangement.yml \
-  -f docker-compose.media-management.yml up -d\
+  -f docker-compose.media-management.yml up -d \
 $@
